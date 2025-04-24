a5767ac706306703ff2ac3b9beeae222c79d2863
01vaibhavnaik <vaibhavnaik32275@gmail.com>
2025-04-24 12:55:05 +0530

    Form

---
+++ Task7/form.js
@@ -0,0 +1,15 @@
+
+
+// function submitForm(){
+//     alert('Hii guys');
+// }
+
+
+function submitForm(){
+    // let value=true;
+    let nameInput = document.formName.nameName.value;
+    let numInput = document.formName.numName.value;
+    alert(nameInput);
+   
+    // return value;
+}
